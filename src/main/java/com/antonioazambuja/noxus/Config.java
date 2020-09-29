b021ac8357ecbf44210ffbce8b9cc6da5b2e730e
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-09-29 00:10:02 -0300

    ref: create redis client and mapping application properties values

--- src/main/java/com/antonioazambuja/noxus/Config.java
+++ src/main/java/com/antonioazambuja/noxus/Config.java
@@ -1,19 +1,35 @@
 package com.antonioazambuja.noxus;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
+import redis.clients.jedis.Jedis;
+
 @Configuration
 public class Config {
+
+	@Value("${configuration.redis.uri}")
+	private String redisURI;
+
+	@Value("${configuration.riot.api.key}")
+	private String riotApiKey;
+
 	@Bean
 	public RestTemplate restTemplate() { return new RestTemplate(); }
 
 	@Bean
+	public Jedis jedisClient() throws URISyntaxException { return new Jedis(new URI(redisURI)); }
+
+	@Bean
 	public HttpHeaders httpHeaders() { 
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("X-Riot-Token", "RGAPI-5adac329-9c25-489f-bf96-5973da9b54a0");
+		headers.set("X-Riot-Token", riotApiKey);
 		return headers;
 	}
 }
